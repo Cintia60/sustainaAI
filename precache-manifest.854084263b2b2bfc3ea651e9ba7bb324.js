5182739db6074b75394b11d5b06b44bd363a0779
Cintia60 <cynthiadalila69@gmail.com>
2024-09-18 22:58:22 +0100

    Updates

---
+++ precache-manifest.854084263b2b2bfc3ea651e9ba7bb324.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d3ff25f76fad49edded68321e1bffd",
+    "url": "/sustainaAI/index.html"
+  },
+  {
+    "revision": "f2f09d160cc506405dae",
+    "url": "/sustainaAI/static/css/main.4ef80183.chunk.css"
+  },
+  {
+    "revision": "996535a59adccb61ae16",
+    "url": "/sustainaAI/static/js/2.ac7ec521.chunk.js"
+  },
+  {
+    "revision": "47fa41051c1997a207c1",
+    "url": "/sustainaAI/static/js/3.f329edd3.chunk.js"
+  },
+  {
+    "revision": "7c7882ec96c3d85cc48a",
+    "url": "/sustainaAI/static/js/4.7a9b5783.chunk.js"
+  },
+  {
+    "revision": "d39cd5cd66f51b7ddc41",
+    "url": "/sustainaAI/static/js/5.3f379f9b.chunk.js"
+  },
+  {
+    "revision": "26578d2a2dd95b9a3af1",
+    "url": "/sustainaAI/static/js/6.ccf31c41.chunk.js"
+  },
+  {
+    "revision": "f2f09d160cc506405dae",
+    "url": "/sustainaAI/static/js/main.0268abbd.chunk.js"
+  },
+  {
+    "revision": "392acac64eb8321b13e4",
+    "url": "/sustainaAI/static/js/runtime~main.1e7bc100.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/sustainaAI/static/media/logo.06e73328.svg"
+  }
+]);
